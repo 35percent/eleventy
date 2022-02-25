dc9d275cc0349041d6735a8e48b90099904076f1
regenwatch <router@router.lan>
2022-02-25 20:11:01 +0000

    eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -8,31 +8,6 @@ module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
-  // --- START, eleventy-img
-  async function imageShortcode(src, alt, sizes) {
-    let metadata = await Image(src, {
-      widths: [300, 600],
-      formats: ["avif", "jpeg"]
-    });
-  
-    let imageAttributes = {
-      alt,
-      sizes,
-      loading: "lazy",
-      decoding: "async",
-    };
-  
-    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-    return Image.generateHTML(metadata, imageAttributes);
-  }
-  
-  module.exports = function(eleventyConfig) {
-    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-    eleventyConfig.addLiquidShortcode("image", imageShortcode);
-    eleventyConfig.addJavaScriptFunction("image", imageShortcode);
-  };
-  // --- END, eleventy-img
-
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
 
